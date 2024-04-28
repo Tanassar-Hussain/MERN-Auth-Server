9bef4a3e7ec119bfd9ca6d7953bb86ee9c6ef887
Tanassar Hussain <trhussain@cinnova.com>
2024-04-29 00:50:19 +0500

    db  setup

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION).then(()=>{
+    console.log('Connected to MongoDB')
+})
+.catch((error) =>{ 
+    console.log(`Error connecting to MongoDB ${error}`)})
 
 const app = express();
 
